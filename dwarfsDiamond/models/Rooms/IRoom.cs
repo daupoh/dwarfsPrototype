3f1654b1655546719c45d1c9d15e87be243d60e8
daupoh <daupoh@DAUPOHPC>
2019-01-28 01:05:09 +0300

    copied my arch

---
+++ dwarfsDiamond/models/Rooms/IRoom.cs
@@ -0,0 +1,22 @@
+﻿using dwarfsDiamond.models.Games;
+using dwarfsDiamond.models.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dwarfsDiamond.models.Rooms
+{
+    interface IRoom : IEntity
+    {
+        uint playersMaxNumbers{get;}
+        uint playersCurrentNumbers { get; }
+        IGame getGameById(string gameID);
+
+        void enterRoom(string playerID);        
+        string createNewGame(); //return game id
+        void playerLeftRoom(string playerID);
+        
+    }
+}
